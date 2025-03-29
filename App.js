a9db67fb94af158f4af8671e572ecc79c733b2f7
Angel Flores <jesusfloresangel@outlook.com>
2025-03-29 09:52:00 -0600

    Correction on App  Changes to be committed: 	modified:   App.js

--- App.js
+++ App.js
@@ -12,13 +12,13 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer initialRouteName="Login" screenOptions={{
+    <NavigationContainer initialRouteName="Home" screenOptions={{
       headerTintColor: '#fff',
     }}>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="SignUp" component={SignUpScreen} options={{  headerShown: false }} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Inicio', headerLeft: null }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false, headerLeft: null }} />
         <Stack.Screen name="Card" component={CardScreen} options={{ title: 'Detalle de tarjeta' }} />
         <Stack.Screen name="Recipes" component={RecipesScreen} options={{ title: 'Recibos' }} />
         <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} options={{ title: 'Detalle de recibo' }} />
